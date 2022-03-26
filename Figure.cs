0f797a67a5309b36effc1535eb204ce2c2a227e7
typaky <93198855+typaky@users.noreply.github.com>
2022-03-26 23:33:31 +0300

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public abstract class Figure
+    {
+        public string Name { get; set; }
+        public abstract double GetArea();
+
+    }
+    class Rectangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+
+    }
+    class Square : Figure
+    {
+        public double Width { get; set; }
+        
+
+        public override double GetArea()
+        {
+            return Width * Width;
+        }
+    }
+    class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius,2);
+        }
+    }
+    class Trangle : Figure
+    {
+        public double Height { get; set; }
+        public double Osnovanie { get; set; }
+
+
+        public override double GetArea()
+        {
+            return 0.5 * Height *Osnovanie;
+        }
+    }
+    class Trapecia : Figure
+    {
+        public double Height { get; set; }
+        public double Osnovanie1 { get; set; }
+        public double Osnovanie2 { get; set; }
+
+
+        public override double GetArea()
+        {
+            return 0.5 * Height * (Osnovanie1 * Osnovanie2);
+        }
+    }
+    class Romb : Figure
+    {
+        
+        public double Diogonal1 { get; set; }
+        public double Diogonal2 { get; set; }
+
+
+        public override double GetArea()
+        {
+            return  Diogonal1 * Diogonal2 / 2;
+        }
+    }
+    class Para : Figure
+    {
+
+        public double Storona { get; set; }
+        public double Height { get; set; }
+
+
+        public override double GetArea()
+        {
+            return Storona * Height;
+        }
+    }
+    class Pati : Figure
+    {
+
+        public double Radius { get; set; }
+       
+
+        public override double GetArea()
+        {
+            return 5 * Math.Pow(Radius,2) * Math.Tan(Math.PI / 5);
+        }
+    }
+    class Dec : Figure
+    {
+
+        public double Radius { get; set; }
+
+
+        public override double GetArea()
+        {
+            return 10 * Math.Pow(Radius, 2) * Math.Tan(Math.PI / 10);
+        }
+    }
+}
+
---
+++ Program(1).cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная №2 - Наследование");
+            Console.WriteLine("Выполнил - Титов Александр\n");
+            Rectangle a = new Rectangle()
+            {
+                Name = "Прямоугольник А",
+                Width = 15.1,
+                Height = 12.8
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", a.Name, a.GetArea());
+            
+            Square b = new Square()
+            {
+                Name = "Квадрат Б",
+                Width = 15.1,
+                
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", b.Name, b.GetArea());
+            
+            Circle c = new Circle()
+            {
+                Name = "Круг В",
+                Radius = 15.1,
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", c.Name, c.GetArea());
+            
+            Trangle t = new Trangle()
+            {
+                Name = "Треугольник Г",
+                Height = 15.1,
+                Osnovanie = 16.1
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", t.Name, t.GetArea());
+            
+            Trapecia g = new Trapecia()
+            {
+                Name = "Трапеция Д",
+                Height = 15.1,
+                Osnovanie1 = 16.1,
+                Osnovanie2 = 12.6
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", g.Name, g.GetArea());
+            
+            Romb r = new Romb()
+            {
+                Name = "Ромб Е",
+                
+                Diogonal1 = 16.1,
+                Diogonal2 = 12.6
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", r.Name, r.GetArea());
+
+            Para p = new Para()
+            {
+                Name = "Паралелограмм Ж",
+
+                Storona = 16.1,
+                Height = 12.6
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", p.Name, p.GetArea());
+            
+            Pati l = new Pati()
+            {
+                Name = "Пятиугольник З",
+
+                Radius = 16.1 
+                
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", l.Name, l.GetArea());
+           
+            Dec m = new Dec()
+            {
+                Name = "Десятиугольник И",
+
+                Radius = 16.1
+
+
+            };
+
+            Console.WriteLine("Название фигуры: {0}\nПлощадь фигруры: {1}", m.Name, m.GetArea());
+
+
+
+
+        }
+    }
+}
